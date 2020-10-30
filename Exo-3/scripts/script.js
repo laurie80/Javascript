dfaf320ffd8c64c2288246d278d2156fb3c4f227
laurie80 <laurie_dodre@outlook.fr>
2020-10-30 15:05:21 +0100

    exercice 3 javascript

---
+++ Exo-3/scripts/script.js
@@ -0,0 +1,7 @@
+let message1 = 'Bonjour, je m\'appelle ';
+let message2 = 'j\'ai ';
+let message3 = ' ans et j\'habite';
+let name = 'Laurie, ';
+ age = 31,
+ city = ' Roisel.';
+alert (message1 + name + message2 + age + message3 + city)
