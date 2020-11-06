1cb81593b186030a572e685a927671b7c1b38e54
laurie80 <laurie_dodre@outlook.fr>
2020-11-06 15:27:33 +0100

    exercice 6 partie 5 avec ajour correction

---
+++ Partie-5/exo-6/scripts/script.js
@@ -0,0 +1,27 @@
+let name = document.getElementById("name");
+let mail = document.getElementById("mail");
+let age = document.getElementById("age");
+
+name.onfocus = function() {
+    document.getElementById("name").style.border = "2px solid black";
+  }
+  
+  name.onblur = function() {
+    document.getElementById("name").style.border = '';;
+  }
+
+  mail.onfocus = function() {
+    document.getElementById("mail").style.border = "2px solid black";;
+  }
+  
+  mail.onblur = function() {
+    document.getElementById("mail").style.border = '';;
+  }
+
+  age.onfocus = function() {
+    document.getElementById("age").style.border = "2px solid black";
+  }
+  
+  age.onblur = function() {
+    document.getElementById("age").style.border = '';
+  }
---
+++ Partie-5/exo-7/scripts/script.js
@@ -0,0 +1,6 @@
+let body = document.getElementById("container");
+
+body.scroll = function(e) {
+    e.preventDefault();
+  
+}
