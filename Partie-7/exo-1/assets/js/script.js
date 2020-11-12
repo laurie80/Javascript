b99697b107f7eb586615a1a99c7a955b3dad007a
laurie80 <laurie_dodre@outlook.fr>
2020-11-12 14:04:41 +0100

    modif fichier js

--- Partie-7/exo-1/assets/js/script.js
+++ Partie-7/exo-1/assets/js/script.js
@@ -1,7 +1,12 @@
+let container = document.getElementById("container");
+
 function calcul() {
   let firstNumber = document.getElementById("firstNumber").value;
   let secondNumber = document.getElementById("secondNumber").value;
   let total = firstNumber * secondNumber;
+  let result = document.createElement('p');
+  result.innerHTML = total;
+  container.appendChild(result);
 }
 
-
+  
