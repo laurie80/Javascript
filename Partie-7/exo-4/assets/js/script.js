667248f273c294a8bd357d865efd62b62b4dfeed
laurie80 <laurie_dodre@outlook.fr>
2020-11-12 16:51:27 +0100

    exercice 4 partie 7

---
+++ Partie-7/exo-4/assets/js/script.js
@@ -0,0 +1,7 @@
+let image = document.getElementById("picture");
+
+// image.onclick = function () {
+//     image.classList.toggle("imgSize");
+// }
+
+image.onclick = () => image.classList.toggle("imgSize");
